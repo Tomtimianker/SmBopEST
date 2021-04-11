cb04255ab45bb564b6497b1ff669178346505037
shaniperl <shaniperl44@gmail.com>
2021-04-11 20:17:10 +0300

    debug on local machine

--- eval_final/evaluation.py
+++ eval_final/evaluation.py
@@ -854,28 +854,6 @@ def build_foreign_key_map_from_json(table):
     return tables
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--gold', dest='gold', type=str)
-    parser.add_argument('--pred', dest='pred', type=str)
-    parser.add_argument('--db', dest='db', type=str)
-    parser.add_argument('--table', dest='table', type=str)
-    parser.add_argument('--etype', dest='etype', type=str)
-    args = parser.parse_args()
-
-    gold = args.gold
-    pred = args.pred
-    db_dir = args.db
-    table = args.table
-    etype = args.etype
-
-    assert etype in ["all", "exec", "match"], "Unknown evaluation method"
-
-    kmaps = build_foreign_key_map_from_json(table)
-    
-
-    evaluate(gold, pred, db_dir, etype, kmaps)
-
 
 
 def evaluate_single(g_str, p_str, db_id, db_dir,  table_file):
@@ -920,3 +898,31 @@ def evaluate_single(g_str, p_str, db_id, db_dir,  table_file):
     
     return evaluator.eval_exact_match(p_sql, g_sql)
 # MODEL_NAME=cloudy-scarlet-bordeaux_gpu3 && python eval_final/evaluation.py  --etype all --db  dataset/database  --table dataset/tables.json --gold experiments/$MODEL_NAME/gold_84.sql --pred experiments/$MODEL_NAME/preds_84.sql
+
+
+#
+# if __name__ == "__main__":
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument('--gold', dest='gold', type=str)
+#     parser.add_argument('--pred', dest='pred', type=str)
+#     parser.add_argument('--db', dest='db', type=str)
+#     parser.add_argument('--table', dest='table', type=str)
+#     parser.add_argument('--etype', dest='etype', type=str)
+#     args = parser.parse_args()
+#
+#     gold = args.gold
+#     pred = args.pred
+#     db_dir = args.db
+#     table = args.table
+#     etype = args.etype
+#
+#     assert etype in ["all", "exec", "match"], "Unknown evaluation method"
+#
+#     kmaps = build_foreign_key_map_from_json(table)
+#
+#
+#     evaluate(gold, pred, db_dir, etype, kmaps)
+
+if __name__ == '__main__':
+    build_foreign_key_map_from_json(
+        '/Users/orlichter/Documents/School/Extra Courses/NLP/SmBopEST/wikisql_dataset/Processed/dev_processed_tables.json')
